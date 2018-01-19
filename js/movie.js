56b0e49b510d27bde94e62c5b2b408143bce7404
aurorex <selva_beat@hotmail.com>
2018-01-19 09:52:45 +0100

    iniciando funcionalidad en el archivo movie.js

---
+++ js/movie.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  var title = $('.title');
+  var sinopsis = $('.sinpsis') ;
+  var year = $('div.year>span');
+  var genre = $('div.genre>span');
+  var director = $('div.director>span');
+  var country = $('div.country>span');
+
+});
