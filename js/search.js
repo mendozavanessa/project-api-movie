7b6bf81758e2701e961528db62c8ef3f2ec88285
aurorex <selva_beat@hotmail.com>
2018-01-19 07:45:53 +0100

    agregando archivo movie.js

--- js/search.js
+++ js/search.js
@@ -1,21 +1,23 @@
 $(document).ready(function() { 
   // creando objeto
   var arrayMovies = [
-    arrayMovieAlegria = ['Duplex', 'Shrek', 'Shrek 2', 'Scary Movie', 'American Pie', 'Click'],
-    arrayMovieAdrenalina = ['Die Hard', 'Rambo', 'Kill Bill: Vol. 1', 'Kill Bill: Vol. 2', 'Terminator 2', 'James Bond 007'],
-    arrayMovieRomance = ['Titanic', '50 First Dates', 'Her', 'The Notebook', 'Ghost', 'The Wedding Singer'],
-    arrayMovieMiedo = ['The Exorcist', 'It', 'The Babadook', 'It Follows', 'Damien: Omen II', 'The Conjuring'],
-    arrayMovieAventura = ['Star Wars: The Last Jedi', 'Harry Potter and the Order of the Phoenix', 'The Lord of the Rings: The Fellowship of the Ring', 'Indiana Jones and the Last Crusade', 'Mad Max', 'Jurassic Park'],
-    arrayMovieReflexión = ['Forrest Gump', 'Eternal Sunshine of the Spotless Mind', 'Little Miss Sunshine', 'Big Fish', 'Silver Linings Playbook', 'The Bucket List'],
+    ['Duplex', 'Shrek', 'Shrek 2', 'Scary Movie', 'American Pie', 'Click'],
+    ['Die Hard', 'Rambo', 'Kill Bill: Vol. 1', 'Kill Bill: Vol. 2', 'Terminator 2', 'James Bond 007'],
+    ['Titanic', '50 First Dates', 'Her', 'The Notebook', 'Ghost', 'The Wedding Singer'],
+    ['The Exorcist', 'It', 'The Babadook', 'It Follows', 'Damien: Omen II', 'The Conjuring'],
+    ['Star Wars: The Last Jedi', 'Harry Potter and the Order of the Phoenix', 'The Lord of the Rings: The Fellowship of the Ring', 'Indiana Jones and the Last Crusade', 'Mad Max', 'Jurassic Park'],
+    ['Forrest Gump', 'Eternal Sunshine of the Spotless Mind', 'Little Miss Sunshine', 'Big Fish', 'Silver Linings Playbook', 'The Bucket List'],
   ];
 
-  var emotionBtn = document.getElementsByTagName('li');
-  console.log(emotionBtn);
+  $('.btn-alegria').on('click', function() {
+    var arrMovieAlegria = arrayMovies[0];
+    var image = $('a.thumbnail>img');
+    console.log(image);
+    for (var i = 0;i < image.length;i++) {
+      arrMovieAlegria.$each() {
+        image.attr('src','src');
+      }
 
-  for (var i = 0;i < emotionBtn.length; i++) {
-    emotionBtn[i].addEventListener('click', function() {
-      arrayMovies[i]
-      alert('hola');
-    });
-  }
+    }
+  });
 });
