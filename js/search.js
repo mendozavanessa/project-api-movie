1592296e468a205d6a5773a2b609a11861e51c7c
aurorex <selva_beat@hotmail.com>
2018-01-19 09:36:28 +0100

    subendo funcion de peliculas

--- js/search.js
+++ js/search.js
@@ -10,14 +10,78 @@ $(document).ready(function() {
   ];
 
   $('.btn-alegria').on('click', function() {
-    var arrMovieAlegria = arrayMovies[0];
-    var image = $('a.thumbnail>img');
-    console.log(image);
-    for (var i = 0;i < image.length;i++) {
-      arrMovieAlegria.$each() {
-        image.attr('src','src');
+    var $image = $('a.thumbnail>img');
+    var url = ' http://www.omdbapi.com/?apikey=79428951&s=' + $(this).attr('data-li');
+    $.ajax({
+      url: url,
+      success: renderMovies
+    });
+    function renderMovies(response) {
+      console.log(response);
+      for (var i = 0;i < $image.length;i++) {
+        console.log(response.Search[i].Poster);
+        $($image[i]).attr('src', response.Search[i].Poster);
       }
-
+    }
+  });
+  $('.btn-aventura').on('click', function() {
+    var $image = $('a.thumbnail>img');
+    var url = ' http://www.omdbapi.com/?apikey=79428951&s=' + $(this).attr('data-li');
+    $.ajax({
+      url: url,
+      success: renderMovies
+    });
+    function renderMovies(response) {
+      console.log(response);
+      for (var i = 0;i < $image.length;i++) {
+        console.log(response.Search[i].Poster);
+        $($image[i]).attr('src', response.Search[i].Poster);
+      }
+    }
+  });
+  $('.btn-miedo').on('click', function() {
+    var $image = $('a.thumbnail>img');
+    var url = ' http://www.omdbapi.com/?apikey=79428951&s=' + $(this).attr('data-li');
+    $.ajax({
+      url: url,
+      success: renderMovies
+    });
+    function renderMovies(response) {
+      console.log(response);
+      for (var i = 0;i < $image.length;i++) {
+        console.log(response.Search[i].Poster);
+        $($image[i]).attr('src', response.Search[i].Poster);
+      }
+    }
+  });
+  $('.btn-reflexion').on('click', function() {
+    var $image = $('a.thumbnail>img');
+    var url = ' http://www.omdbapi.com/?apikey=79428951&s=' + $(this).attr('data-li');
+    $.ajax({
+      url: url,
+      success: renderMovies
+    });
+    function renderMovies(response) {
+      console.log(response);
+      for (var i = 0;i < $image.length;i++) {
+        console.log(response.Search[i].Poster);
+        $($image[i]).attr('src', response.Search[i].Poster);
+      }
+    }
+  });
+  $('.btn-romance').on('click', function() {
+    var $image = $('a.thumbnail>img');
+    var url = ' http://www.omdbapi.com/?apikey=79428951&s=' + $(this).attr('data-li');
+    $.ajax({
+      url: url,
+      success: renderMovies
+    });
+    function renderMovies(response) {
+      console.log(response);
+      for (var i = 0;i < $image.length;i++) {
+        console.log(response.Search[i].Poster);
+        $($image[i]).attr('src', response.Search[i].Poster);
+      }
     }
   });
 });
